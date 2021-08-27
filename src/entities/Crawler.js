b40158739f0301d7d0289c4ad21021edb2e821bf
Ed de Almeida <edvaldoajunior@gmail.com>
2021-08-27 15:14:14 -0300

    Basic single_request method implemented

--- src/entities/Crawler.js
+++ src/entities/Crawler.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const RegisteredCrawlers = require('./RegisteredCrawlers');
 
 class Crawler {
@@ -8,15 +10,36 @@ class Crawler {
         this.crawlerSpec = {};
         this.baseURL = null;
         this.method = null;
+        this.pathToList = null;
+        this.data = null;
     }
 
-    run_crawler() {
-        console.log("Running the crawler...");
+    crawler_fetch_data() {
+        fetch(this.baseURL)
+            .then(res => res.json())
+            .then(res => {
+                this.data = res;
+                this.pathToList.forEach(element => {
+                    this.data = this.data[element];
+                    console.log(this.data);
+                });
+                console.log(this.data);
+            })
+    }
+
+    single_request_run() {
+        this.crawler_fetch_data();
+    }
+
+    crawler_run() {
+        if (this.method === 'single_request') {
+            this.single_request_run();
+        }
     }
 
     process() {
         try {
-            
+
             if (!this.regCrawlers.isRegistered(this.name)) {
                 throw Error('Not a registered crawler: ' + this.name);
             }
@@ -24,8 +47,9 @@ class Crawler {
             this.crawlerSpec = this.regCrawlers.crawlerSpec(this.name);
             this.baseURL = this.crawlerSpec.baseURL;
             this.method = this.crawlerSpec.method;
+            this.pathToList = this.crawlerSpec.pathToList.split('|');
 
-            this.run_crawler(); 
+            this.crawler_run();
         } catch (e) {
             console.log(e.message);
             process.exit(9); // Exiting - Invalid parameter
