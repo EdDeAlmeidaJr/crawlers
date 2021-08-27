6908c745040e9146311e4527d558ea9fea0ffabc
Ed de Almeida <edvaldoajunior@gmail.com>
2021-08-27 07:18:26 -0300

    Process correct exit code

--- src/entities/Crawler.js
+++ src/entities/Crawler.js
@@ -5,16 +5,31 @@ class Crawler {
     constructor(name) {
         this.name = name;
         this.regCrawlers = new RegisteredCrawlers();
+        this.crawlerSpec = {};
+        this.baseURL = null;
+        this.method = null;
+    }
+
+    run_crawler() {
+        console.log("Running the crawler...");
     }
 
     process() {
         try {
+            
             if (!this.regCrawlers.isRegistered(this.name)) {
                 throw Error('Not a registered crawler: ' + this.name);
             }
+
+            this.crawlerSpec = this.regCrawlers.crawlerSpec(this.name);
+            this.baseURL = this.crawlerSpec.baseURL;
+            this.method = this.crawlerSpec.method;
+
+            this.run_crawler(); 
         } catch (e) {
             console.log(e.message);
-            process.exit();
+            process.exit(9); // Exiting - Invalid parameter
+
         }
     }
 
